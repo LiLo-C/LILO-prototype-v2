73f630cdb45078a28b9a391dd07da380a53c6945
camellion <ravenspear460@gmail.com>
2026-09-16 23:02:28 +0700

    Initial Commit

---
+++ v2/v2App.swift
@@ -0,0 +1,17 @@
+//
+//  v2App.swift
+//  v2
+//
+//  Created by Calzy Akmal Indyramdhani on 16/09/26.
+//
+
+import SwiftUI
+
+@main
+struct v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
